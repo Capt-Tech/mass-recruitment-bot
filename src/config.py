e0b775a0af88cf9bec90cdd04a508b93401d90e9
Jonathan Loh <jonathanloh02@gmail.com>
2024-07-12 22:24:40 +0800

    Update for deployment

--- src/config.py
+++ src/config.py
@@ -13,6 +13,7 @@ def read_dotenv():
             "BASE_PATH": os.getenv("BASE_PATH", "./data"),
             "WEBHOOK_SECRET": os.getenv("WEBHOOK_SECRET"),
             "WEBHOOK_URL": os.getenv("WEBHOOK_URL"),
+            "PORT": os.getenv("PORT", "3000"),
             "PRODUCTION": os.getenv("PRODUCTION") == "True",
             "ADMIN_USERNAME": os.getenv("ADMIN_USERNAME", "").split(";"),
             "DEVELOPERS": os.getenv("DEVELOPERS", "").split(";"),
